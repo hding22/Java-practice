3f38327907cf54fb638f8c4ddaa7d56e7b8a837f
Hong Ding <hong.ding@marquette.edu>
2018-07-14 14:38:32 -0500

    Create do-while.java

---
+++ purple book/do-while.java
@@ -0,0 +1,25 @@
+package com.company;
+//do-while
+
+import java.util.*;
+
+public class App4_8 {
+    public static void main(String[] args) {
+        int n, i = 1, sum = 0;
+        Scanner buf = new Scanner(System.in);
+        do {
+            System.out.print("please enter positive integer");
+            n = buf.nextInt();
+        } while (n <= 0);
+
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("1+2+...+" + n + "=" + sum);
+
+    }
+}
+
+
