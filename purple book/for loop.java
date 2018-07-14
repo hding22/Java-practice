fb6c0d99f191127b011c7b42925f29020c3081a5
Hong Ding <hong.ding@marquette.edu>
2018-07-14 15:24:56 -0500

    Create for loop.java

---
+++ purple book/for loop.java
@@ -0,0 +1,20 @@
+package com.company;
+//how to count cumulative from 1 to 10
+
+public class App4_11 {
+    public static void main(String[] args) {
+        int i, n = 10, s = 0;
+        for (i = 1; i <= n; i++)
+            s = s + i;
+        System.out.println("Sum= 1+...+" + n + "=" + s);
+        s = 0;
+        System.out.print("Sum= ");
+        for (i = n; i > 1; i--) {
+            s += i;
+            System.out.print(i + "+");
+
+        }
+        System.out.println(i + "=" + (s + i));
+    }
+
+}
